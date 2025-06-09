2480ecfb172b2ead909ab4af77798c66af956f35
Urban <urban.mutagen@gmail.com>
2025-06-09 17:49:50 +0300

    the END

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import { getAllContacts, getContactById } from './services/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 const port = Number(getEnvVar('PORT'));
 
@@ -14,11 +15,48 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use((request, response, next) => {
+  // ...........................................................................
+  app.get('/contacts/contacts', async (request, response) => {
+    const contacts = await getAllContacts();
+    response.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/contacts/:contactId', async (request, response, next) => {
+    const { contactId } = request.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      response.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+    response.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  //  .............................................................................
+  app.use((error, req, res, next) => {
+    const { status = 500, message = 'Something went wrong' } = error;
+    res.status(status).json({
+      status,
+      message,
+    });
+  });
+
+  app.use((request, response) => {
     response.status(404).json({
       message: 'Not found',
     });
   });
+
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
